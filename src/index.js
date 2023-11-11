898937701eaa2a971b46fc65280f1909b9f20658
Daryna Holikova <dgolikova0@gmail.com>
2023-11-11 11:02:46 +0000

    edit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
      <Provider store={store}>
      <PersistGate persistor={persistor}>
     <App />
